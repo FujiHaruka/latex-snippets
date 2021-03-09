98722f0726345eb22b5a95aa9ebafd1c4719666a
FujiHaruka <fizzyjohn1427@gmail.com>
2021-03-09 23:28:11 +0900

    style

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.css"
 import { useEffect, useRef, useState } from 'react';
 import { useMathJaxContext } from './MathJaxLoader';
 import Canvg from 'canvg'
@@ -5,22 +6,33 @@ import Canvg from 'canvg'
 function App() {
   const { ready } = useMathJaxContext()
   const [tex, setTex] = useState('ax^2=c')
-  const canvas = useRef()
+  const [imgSrc, setImgSrc] = useState('')
+  const canvas = useRef(null)
   useEffect(() => {
     if (!ready) {
       return
     }
-    window.MathJax.tex2svgPromise(tex, { containerWidth: 200, display: false }).then((tex) => {
+    window.MathJax.tex2svgPromise(tex, { em: 32, ex: 16, scale: 2, display: false }).then((tex) => {
       const ctx = canvas.current.getContext('2d')
       Canvg.fromString(ctx, tex.outerHTML).start()
+      const dataUrl = canvas.current.toDataURL()
+      setImgSrc(dataUrl)
     })
   }, [ready, tex])
   return (
     <div className="App">
-      <div>
-        <textarea value={tex} onChange={(e) => setTex(e.target.value)} />
+      <header className="App-header">
+        <h2 className="App-header-title">LaTeX to Image</h2>
+      </header>
+      <div className="App-body">
+      <div className="App-body-col">
+        <textarea className="App-textarea" value={tex} onChange={(e) => setTex(e.target.value)} />
       </div>
-      <canvas ref={canvas} width="200" height="200"></canvas>
+      <div className="App-body-col">
+        <img src={imgSrc} width="200" height="200" alt="" />
+        <canvas className="App-canvas" ref={canvas} width="200" height="200"></canvas>
+      </div>
+      </div>
     </div>
   );
 }
